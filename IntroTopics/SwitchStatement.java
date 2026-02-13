e1e83e4dd655237790924d52fdfbfbe05acf3ddc
Saniketbaisoya <sanibaisoya001@gmail.com>
2026-02-13 18:41:13 +0530

    switch syntax

---
+++ IntroTopics/SwitchStatement.java
@@ -0,0 +1,30 @@
+
+
+public class SwitchStatement {
+    public static void main(String[] args) {
+
+        int number = 50;
+        String size;
+        switch (number){
+            case 29:
+                size = "small";
+                break;
+
+            case 42:
+                size = "medium";
+                break;
+            case 48:
+                size = "large";
+                break;
+            case 50:
+                size = "extra large";
+                break;
+            default:
+                size = "unknown";
+                break;
+
+        }
+
+        System.out.println("size is : " + size);
+    }
+}
