9af2f215fd61184561127f5b327573244c4c725e
Saniketbaisoya <sanibaisoya001@gmail.com>
2026-02-14 15:11:51 +0530

    adding

--- IntroTopics/Output.java
+++ IntroTopics/Output.java
@@ -5,7 +5,7 @@ public class Output {
         /**
          * Now System is class , out is field, println is a method inside out
          * println mai n -> \n joki cursor ko next line mai bhi lekr ayega after print whole things from the println
-         * print mai koi n nhi hai jiski vje se next line mai cursor nhi gya mai 1. print hone ke baad 2. print hogya
+         * print mai koi n nhi hai jiski vje se next line mai cursor nhi gya, 1. print hone ke baad 2. print hogya
          */
         System.out.println("1. println");
         System.out.println("2. println");
