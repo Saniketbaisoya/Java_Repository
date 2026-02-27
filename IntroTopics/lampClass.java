fada181a3d3c1d1d4966b01409aa97cfba6a7995
Saniketbaisoya <sanibaisoya001@gmail.com>
2026-02-27 17:30:13 +0530

    chaning the blueprint of lampClass

--- IntroTopics/lampClass.java
+++ IntroTopics/lampClass.java
@@ -3,10 +3,12 @@ public class lampClass {
     boolean isOn;
 
     void turnOn(){
+        isOn = true;
         System.out.println("Lamp is on !!");
     }
 
     void turnOff(){
+        isOn = false;
         System.out.println("Lamp is off !!");
     }
 }
